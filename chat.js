08af9f8d19933927224128534b62b543ebb93d85
koyouavang94 <50379999+koyouavang94@users.noreply.github.com>
2019-07-22 23:49:43 -0500

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,39 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("p");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+    document.getElementById("messagesList").style.color = blue;
+});
+
+connection.on("ReceiveDM", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("p");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList1").appendChild(li);
+    document.getElementById("messagesList1").style.color = green;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
